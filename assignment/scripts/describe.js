09b10434a9a985af717a8b52bcd17cd12602dca3
YaniraHagstrom <yanirahagstrom@gmail.com>
2022-07-22 11:59:32 -0500

    part 2 submission

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,9 +26,8 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// We declare a variable called 'name' and set it equal to string value 'Dane'.
+// We use a conditional if statement to check is the variable 'name' equals string value 'Mary' and if it does we console log 'Hi, Mary'. Otherwise, if the variable 'name' does not equal 'Mary', we console log 'How do you do?'
 //
 
 //CODE
@@ -45,9 +44,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// We declare variables named 'secret' and 'code' and set 'code' equal to number 123.
+// We use a conditional if statement to check the value of 'code'. If 'code' equals 123 then we set 'secret' equal to 'super' and 'code' equal to double itself. 
+// 'code' now equals 246
+// We use another conditional if statement to check If 'code' is greater than 250. 
+// Since code is not greater than 250, we console log 'super'.
 //
 
 //CODE
@@ -70,10 +71,10 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We declare variables isStudent = true, age = 34 and zip = 55407.
+// We use a conditional if statement. If isStudent is true and zip is greater than 80000 then we console log 'You're a student on the West Coast!'
+// If this conditional is not true then we check if isStudent is false or age is less than 30. If either of these conditions are true we console log 'What are your hobbies?'.
+// If neither conditions are true then we check if isStudent is true and is so we console log 'Welcome to Prime'
 
 //CODE
 /*
@@ -129,11 +130,13 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+// FIX - colorOne should be set to 'blue' and colorTwo should be set to 'red'. 
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
+// FIX - colorTwo should be set to 'purple'. should include colorTwo = 'purple'
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -144,9 +147,12 @@ if (mix === true) {
 // -- they are so we console.log 'throw away the food!'
 
 /*
+
+// FIX - the constant 'time' should be a variable.
 let temp = 40;
 const time = 4;
 
+// FIX - the conditional statement should have && instead of || to create an and statement instead of an or statement.
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -156,10 +162,12 @@ if (temp > 39 || time >= 4) {
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
 
+//FIX - 'minAge' should be a variable not a constant.
 /*
 let age = 21;
 const minAge = 21;
 
+//FIX - since 'age' == 'minAge' then we should be console logging 'no entry'.
 if(minAge <= age) {
   console.log('no entry');
 } else {
